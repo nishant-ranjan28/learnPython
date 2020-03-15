e30e1c9d4f9aaace7f88fc31b63c2e4043ccef1a
HeyCamCam <ansiblenotify+gh@gmail.com>
2020-03-15 15:49:11 -0400

    this is fun

--- basic/app.py
+++ basic/app.py
@@ -1,2 +1,14 @@
+#!/usr/bin/env python3
+
 print('I am Nishant')
 print('*' * 10)
+
+text = 'We are learning. Lets see what we can do.'
+print(text)
+
+print('Lets reverse the above statement')
+
+stringlength=len(text)
+reversedString=text[stringlength::-1]
+
+print(reversedString)
