547d9e18848bd6a8c71b09bef53f66f7df521fe6
nishant-ranjan28 <nishantranjan78@gmail.com>
2020-03-18 19:01:55 +0530

    datatypes, variables examoples

---
+++ basic/datatypes.py
@@ -0,0 +1,13 @@
+failed_subjects= 2
+name= 'John'
+print('Dear Mrs Badger')
+print('Your son  ' + name + '  is failing ' + str(failed_subjects) + ' subjects.')
+print(name + ' will need to redo  ' + str(failed_subjects) + '  courses.')
+name = 'Eric'
+print(name + ' is doing well in geography.')
+
+
+print(type('hello'))
+print(type(1))
+print(type(2.5))
+print(type(True))
---
+++ basic/variablesAndDatatypes.py
@@ -0,0 +1,8 @@
+print('veriable and data types')
+
+item_name = 'widgets'
+price = 12.90
+inventory = 100
+is_in_inventory = True
+
+print(item_name, price, inventory)
